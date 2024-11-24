86cd3021ce2c78a995f086861cef92eea89117f4
danusan-s <141602943+danusan-s@users.noreply.github.com>
2024-11-24 22:41:33 +0800

    Add vim mappings and options

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,32 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "J", "mzJ`z")
+
+-- Do this in cinnamon plugin
+-- map("n", "<C-u>", "<C-u>zz")
+-- map("n", "<C-d>", "<C-d>zz")
+
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+-- next greatest remap ever : asbjornHaland
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+
+map({ "n", "v" }, "<leader>d", [["_d]])
+
+map("n", "Q", "<nop>")
+map("n", "q:", "<nop>")
+
+map("n", "<C-k>", "<cmd>cnext<CR>zz")
+map("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]])
+
+-- Keymap for opening weblinks
+map("n", "gx", [[:execute '!open ' . shellescape(expand('<cfile>'), 1)<CR>]])
